d16e7e4f82c2627af824f319581d88d86b555aee
Diogo Parente <diogoparente.mypersonal@gmail.com>
2023-08-28 10:26:14 +0100

    mk ii: refactor micro-frontend
    We we're using a hook that was not really re-used, so we extracted
    its logic and integrated it directly with the micro-frontend component

--- webpack-host/src/components/micro-frontend.tsx
+++ webpack-host/src/components/micro-frontend.tsx
@@ -1,5 +1,68 @@
-import useDynamicComponent from "../hooks/use-dynamic-component";
+import React from "react";
+import script from "scriptjs";
 import { useMicrofrontend } from "../context/micro-frontends.context";
+
+import { useLocation } from "react-router-dom";
+
+export interface ModuleProps {
+  scope: string;
+  version: string;
+  props?: any;
+}
+
+const URL = "http://localhost:3000/remote.umd.js"; // Modify as needed
+
+const useModule = ({ scope }: Pick<ModuleProps, "scope">) => {
+  const [module, setModule] = React.useState<any>();
+  const [error, setError] = React.useState<string | null>("");
+
+  React.useEffect(() => {
+    script(URL, () => {
+      const target = window[scope];
+      if (target) {
+        setModule(target);
+        setError(null);
+      } else {
+        setError(`Cannot load module ${scope} at ${URL}`);
+        setModule(null);
+      }
+    });
+  }, [scope]);
+
+  return { module, error };
+};
+
+const MicroFrontend = ({ module, props }: { module: any; props: any }) => {
+  const { pathname } = useLocation();
+
+  const { Element } = module.default.find(
+    ({ path }: { Element: any; path: string }) => path === pathname
+  );
+  const TargetMicroFrontend = () => <Element {...props} />;
+  return <TargetMicroFrontend />;
+};
+
+const ModuleLoader = ({ scope, props }: ModuleProps) => {
+  const { module, error } = useModule({ scope });
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (module?.default) {
+    return <MicroFrontend module={module} props={props} />;
+  }
+
+  return null;
+};
+
+const useDynamicComponent = (props: ModuleProps) => {
+  return (
+    <React.Suspense fallback={""}>
+      <ModuleLoader {...props} />
+    </React.Suspense>
+  );
+};
 
 const Microfrontend = ({ microId = "remote" }: { microId: string }) => {
   const microfrontend = useMicrofrontend({ id: microId });
@@ -14,7 +77,7 @@ const Microfrontend = ({ microId = "remote" }: { microId: string }) => {
     props,
   });
 
-  return Component ? Component : null;
+  return Component ?? null;
 };
 
 export default Microfrontend;
